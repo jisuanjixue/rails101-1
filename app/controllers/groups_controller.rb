f32d720b23e892205cfa63c8e8c3eace71a54a39
like <20783506632@qq.com>
2017-03-19 22:28:15 +0800

    add user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create]
   def index
     @groups = Group.all
   end
@@ -18,17 +18,20 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
   if @group.save
+  redirect_to groups_path
   else
-    redirect_to groups_path
+    render :new
   end
 end
 
   def update
     @group = Group.find(params[:id])
   if @group.update(group_params)
+   redirect_to groups_path
   else
-    redirect_to groups_path
+   render :edit  
   end
 end
 
