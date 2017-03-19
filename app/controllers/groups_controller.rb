1c91048dd3cc507f69b7c3284c0b6bbc68dc2876
like <20783506632@qq.com>
2017-03-19 17:47:41 +0800

    implement CRUD

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,45 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.new(group_params)
+  if @group.save
+  else
+    redirect_to groups_path
+  end
+end
+
+  def update
+    @group = Group.find(params[:id])
+  if @group.update(group_params)
+  else
+    redirect_to groups_path
+  end
+end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+  flash[:alert] = "Group deleted"
+   redirect_to groups_path
+end
+
+private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
